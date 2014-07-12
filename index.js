d5c1c46da65d7ce2b3dbc0b4f28cc091134ca532
Zyluss <Zyluss@users.noreply.github.com>
2014-07-12 15:45:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ $("#page").css("top",$(".header").outerHeight());
 $(window).resize(function(){
   $("#page").css("top",$(".header").outerHeight());
 });
+$( document ).click(function() {
+  $( "#header" ).toggle( "fold" );
+});
