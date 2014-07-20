f03d35cd69d18282cf0527c9e493eb3422053fb9
Zyluss <Zyluss@users.noreply.github.com>
2014-07-20 13:17:14 +0100

    Create jQuery.js

---
+++ jQuery.js
@@ -0,0 +1,9 @@
+$( ".header" ).ready(function() {
+  $( ".header" ).animate({
+    width: "70%",
+    opacity: 1,
+    marginLeft: "0.6px",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+});
