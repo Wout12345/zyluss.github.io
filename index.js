c149a6a073ebf295c5fa70fde1a38973902a519f
Zyluss <Zyluss@users.noreply.github.com>
2014-07-22 17:40:41 +0100

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-$("#page").css("top",$(".header").outerHeight());
-
-$(window).resize(function(){
-  $("#page").css("top",$(".header").outerHeight());
-});
-$( document ).click(function() {
-  $( "#header" ).toggle( "fold" );
-});
