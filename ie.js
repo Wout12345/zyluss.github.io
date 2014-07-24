f305baed4d9ba3277c76027d9f5575b65628ffd2
Zyluss <Zyluss@users.noreply.github.com>
2014-07-24 15:08:32 +0100

    Created
    For stuff'n things

---
+++ ie.js
@@ -0,0 +1,29 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html class=""> <!--<![endif]-->
+<head>
+
+
+
+
+
+(function ($) {
+    "use strict";
+
+    // Detecting IE
+    var oldIE;
+    if ($('html').is('.ie6, .ie7, .ie8')) {
+        oldIE = true;
+    }
+
+    if (oldIE){
+alert('Please do not use internet explorer due to its incompatibility and low performance, instead use: Opera, Chrome, Firefox or Safari, Thank you - Web developers');
+
+    } else {
+        // ..And here's the full-fat code for everyone else
+    }
+
+}(jQuery));
