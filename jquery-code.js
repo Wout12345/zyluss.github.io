931b028137e0e681b0fcda3474343bd0433f8665
Zyluss <Zyluss@users.noreply.github.com>
2014-08-01 09:45:37 +0100

    Update jquery-code.js

--- jquery-code.js
+++ jquery-code.js
@@ -1,15 +1,1 @@
-$(window).load(function() {
-  $( ".header" ).animate({
-    width: "70%",
-    opacity: 1,
-    marginLeft: "0.6px",
-    fontSize: "3em",
-    borderWidth: "10px"
-  }, 1500 );
-});
 
-$(window).ready(function() {
-  $( "div" ).show( "slow", function() {
-    // Div show
-  });
-});
